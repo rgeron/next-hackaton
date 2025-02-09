180a433c3a19fa1ac680315acc8c0591e236db31
rgeron <raphaelgeron123@gmail.com>
2025-02-09 22:47:28 +0100

    yep

--- components/hero.tsx
+++ components/hero.tsx
@@ -5,14 +5,12 @@ export function Hero() {
   return (
     <div className="flex flex-col gap-8 items-center py-16">
       <h1 className="text-4xl md:text-5xl font-bold text-center">
-        HEC Paris Hackathon 2024
+        HEC Paris Hackathon 2025 
       </h1>
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-2xl text-center">
         Find your dream team for the{" "}
-        <span className="font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
-          April 5-6 Hackathon
-        </span>{" "}
-        at HEC Paris Campus
+        <span className="font-bold bg-clip-text">April 5-6 Hackathon</span> at
+        HEC Paris Campus
       </p>
       <p className="text-xl text-muted-foreground max-w-xl text-center">
         Connect with talented developers, designers, and business minds.
